f1653181b1f54406d6f57502f8cda5906572f49a
wangtianhua <wangtianhua1@xiaohongshu.com>
2025-06-13 18:30:51 +0800

    6.13 项目上线并测试 oss图片解析失败

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,12 +10,17 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3002,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true
       }
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
     }
   }
 }) 
